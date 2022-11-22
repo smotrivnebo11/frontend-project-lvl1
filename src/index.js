841e4b5ed3bbea5d7f4f16e5da0bcb5c2ebc6aba
Elena <le.poltanova@yandex.ru>
2022-11-22 18:30:10 +0300

    Corrected index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import readlineSync from 'readline-sync';
 
 const countRounds = 3;
 
-  brainGames(description, getAnswerAndQuestion);
-  const brainGames = (description, ) => {
+  const brainGames = (description, getAnswerAndQuestion) => {
   // 1. приветствие
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
